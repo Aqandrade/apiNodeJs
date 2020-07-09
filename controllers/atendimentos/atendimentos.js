2270274c00582f551c366681bdec3c8b0186222a
Alisson <alisson.andrade@codding.com.br>
2020-07-08 22:41:11 -0300

    Adicionando delete e retorno de atendimentos ao atualizar e inserir.

--- controllers/atendimentos/atendimentos.js
+++ controllers/atendimentos/atendimentos.js
@@ -19,6 +19,12 @@ module.exports = app => {
         Atendimento.altera(id,valores,res);
     });
 
+    app.delete('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id);
+
+        Atendimento.deleta(id,res);
+    });
+
     app.post('/atendimentos',(req,res) => {
         const atendimento = req.body;
 
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -41,7 +41,7 @@ class Atendimento {
                 if(erro){
                     res.status(400).json(erro);
                 } else {
-                    res.status(201).json(resultados)
+                    res.status(201).json(atendimento)
                 }
             });
 
@@ -93,9 +93,22 @@ class Atendimento {
             if(erro){
                 res.status(400).json(erro);
             } else {
-                res.status(200).json(resultados);
+                res.status(200).json({...valores,id});
             }
         });
+    }
+
+    deleta(id,res){
+        const sql = `DELETE FROM atendimentos WHERE id = ${id}`;        
+
+        conexao.query(sql,(erro,resultados) => {
+            if(erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json({id});
+            }
+        });
+
     }
 }
 
