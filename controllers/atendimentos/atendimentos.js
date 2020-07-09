b7f6a525f38273a862bac80f760e563ba1bbd7aa
Alisson <alisson.andrade@codding.com.br>
2020-07-08 22:14:30 -0300

    Adicionando buscaPorId e busca no atendimentos

--- controllers/atendimentos/atendimentos.js
+++ controllers/atendimentos/atendimentos.js
@@ -2,8 +2,14 @@ const Atendimento = require('../../models/atendimentos');
 
 module.exports = app => {
     app.get('/atendimentos', (req,res) => { 
-        res.end("Você está na /atendimento");
+        Atendimento.lista(res);
     });
+
+    app.get('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id);
+
+        Atendimento.buscaPorId(id,res);
+    })
 
     app.post('/atendimentos',(req,res) => {
         const atendimento = req.body;
